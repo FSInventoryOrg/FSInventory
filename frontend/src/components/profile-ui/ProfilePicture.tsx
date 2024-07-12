c4687d59feddcde2b580fd3f71c7a6c1d8fb43bd
Alpha <faipacada@gmail.com>
2024-07-12 22:20:52 +0800

    Open image using hidden input and convert img to base64 and call onSave prop

--- frontend/src/components/profile-ui/ProfilePicture.tsx
+++ frontend/src/components/profile-ui/ProfilePicture.tsx
@@ -2,32 +2,31 @@ import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
 import { Camera } from "lucide-react";
 import { UserIcon } from "../icons/UserIcon";
-import { useForm } from "react-hook-form";
-import { ChangeEvent, useState, useRef } from "react";
+import { ChangeEvent, useRef } from "react";
 
 interface ProfilePictureProps {
-  src: string;
+  src: string | undefined;
   userId: string;
-  onSave: () => null;
+  onSave: (data: object) => void;
 }
 
 const ProfilePicture = ({ src, userId, onSave }: ProfilePictureProps) => {
   const fileInputRef = useRef<HTMLInputElement | null>(null);
-  const [img, setImg] = useState(src);
-  const { handleSubmit, register } = useForm();
-  const { ref, ...rest } = register("firstName");
   const handleUpload = (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     if (file) {
-      setImg(URL.createObjectURL(file));
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const base64String = reader.result;
+        const payload = { src: base64String, filename: file.name, userId };
+        onSave(payload);
+      };
+      reader.readAsDataURL(file);
     }
   };
 
-  const onSubmit = () => {
-    onSave();
-  };
-
-  const handleButtonClick = () => {
+  const handleButtonClick = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
     if (fileInputRef.current != null) {
       fileInputRef.current.click();
     }
@@ -38,7 +37,7 @@ const ProfilePicture = ({ src, userId, onSave }: ProfilePictureProps) => {
       <div className="flex flex-col items-center">
         <div className="relative h-64 w-64 bg-muted border-border border rounded-full justify-center items-center flex">
           <Avatar className="w-64 h-64">
-            <AvatarImage src={img} />
+          <AvatarImage src={src} />
             <AvatarFallback>
               <UserIcon
                 size={220}
@@ -48,16 +47,11 @@ const ProfilePicture = ({ src, userId, onSave }: ProfilePictureProps) => {
           </Avatar>
           <input
             type="file"
-            {...rest}
             style={{ display: "none" }}
-            ref={(e) => {
-              ref(e);
-              fileInputRef.current = e; // you can still assign to ref
-            }}
+          ref={fileInputRef}
             accept="image/*"
             onChange={handleUpload}
           />
-
           <Button
             className="absolute rounded-full right-2 bottom-2 border-4 h-12 w-12 border-bg-accent items-center justify-center"
             size="icon"
