b918724b108a453aece44161df14270b9bc1b584
Reynand Hingcayog <rhnaney@gmail.com>
2024-07-19 16:52:08 +0000

    Merge branch 'fix-blank-employee-name' into 'develop'
    Trim Whitespaces
    
    See merge request full-scale-internship/stockpilot!27
    
    (cherry picked from commit babe89d45f11f33b373d082e541e6938e10fb827)
    
    6f742b32 add trimming of whitespaces when submitting forms
    
    Co-authored-by: Reynand Hingcayog <rhnaney@gmail.com>

--- frontend/src/schemas/AddEmployeeSchema.ts
+++ frontend/src/schemas/AddEmployeeSchema.ts
@@ -1,11 +1,11 @@
 import { z } from 'zod';
 
 export const EmployeeSchema = z.object({
-  code: z.string().min(1, "Asset code is required"),
-  firstName: z.string().min(1, 'Cannot be empty'),
-  middleName: z.string().optional(),
-  lastName: z.string().min(1, 'Cannot be empty'),
-  position: z.string().min(1, 'Cannot be empty'),
+  code: z.string().trim().min(1, 'Asset code is required'),
+  firstName: z.string().trim().min(1, 'Cannot be empty'),
+  middleName: z.string().trim().optional(),
+  lastName: z.string().trim().min(1, 'Cannot be empty'),
+  position: z.string().trim().min(1, 'Cannot be empty'),
   startDate: z.date(),
   isActive: z.boolean(),
 });
