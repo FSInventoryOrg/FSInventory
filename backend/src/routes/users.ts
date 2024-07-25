a28f020027857ec46d5dd208a885dc77c06e762a
Reynand Hingcayog <rhnaney@gmail.com>
2024-07-25 11:15:41 +0000

    completed the reset password backend side

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -4,44 +4,22 @@ import User from '../models/user.schema';
 import { UserType } from '../models/user.schema';
 import { check, validationResult } from 'express-validator'
 import verifyToken from '../middleware/auth';
+import OTPTransaction, { OTPTransactionType } from '../models/otptransactions.schema';
+import mongoose, { mongo } from 'mongoose';
+import { generateHash, generateOTP } from '../utils/common';
 
 const router = express.Router();
 
-/**
- * @openapi
- * /api/users/register:
- *  post:
- *    tags:
- *      - User
- *    summary: Registers a new user
- *    requestBody:
- *      required: true
- *      content:
- *        application/json:
- *          schema:
- *            $ref: '#/components/schemas/User'
- *    responses:
- *      200:
- *        description: User registered successfully
- *        content:
- *          application/json:
- *            schema:
- *              $ref: '#/components/schemas/User' 
- *      400:
- *        description: Invalid request body or email address already registered
- *      500:
- *        description: Internal server error
- */
-router.post("/register", [ 
-    check("email", "Email is required").isEmail(),
-    check("password", "Password with 8 or more characters required").isLength({ min: 8 }),
-    check("firstName", "First name is required").isString(),
-    check("lastName", "Last name is required").isString(),
-    check("role", "Role is required").isString(),
-  ],
+router.post("/register", [
+  check("email", "Email is required").isEmail(),
+  check("password", "Password with 8 or more characters required").isLength({ min: 8 }),
+  check("firstName", "First name is required").isString(),
+  check("lastName", "Last name is required").isString(),
+  check("role", "Role is required").isString(),
+],
   async (req: Request, res: Response) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()) {
+    if (!errors.isEmpty()) {
       return res.status(400).json({ message: errors.array() })
     }
     try {
@@ -58,11 +36,11 @@ router.post("/register", [
       await user.save();
 
       const token = jwt.sign(
-        { 
+        {
           userId: user.id,
           role: user.role
-        }, 
-        process.env.JWT_SECRET_KEY as string, 
+        },
+        process.env.JWT_SECRET_KEY as string,
         { expiresIn: "1d" }
       );
 
@@ -81,34 +59,13 @@ router.post("/register", [
   }
 )
 
-/**
- * @openapi
- * /api/users:
- *  get:
- *    tags:
- *      - User
- *    summary: Get current user details via auth_token in cookies
- *    responses:
- *      200:
- *        description: User found
- *        content:
- *          application/json:
- *            schema:
- *              $ref: '#/components/schemas/User'
- *      404:
- *        description: User not found
- *      500:
- *        description: Internal server error
- *    security:
- *      - bearerAuth: []
- */
 router.get("/", verifyToken, async (req: Request, res: Response) => {
   try {
     const token = req.cookies.auth_token;
     const decodedToken: any = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
     const userId = decodedToken.userId;
 
-    const user: UserType | null = await User.findById(userId, { password: false});
+    const user: UserType | null = await User.findById(userId, { password: false });
 
     if (!user) {
       return res.status(404).json({ message: "User not found" });
@@ -122,44 +79,99 @@ router.get("/", verifyToken, async (req: Request, res: Response) => {
   }
 });
 
-/**
- * @openapi
- * /api/users:
- *  put:
- *    tags:
- *      - User
- *    summary:  Update user details
- *    responses:
- *      200:
- *        description: User found
- *        content:
- *          application/json:
- *            schema:
- *              $ref: '#/components/schemas/User'
- *      404:
- *        description: User not found
- *      500:
- *        description: Internal server error
- *    security:
- *      - bearerAuth: []
- */
 router.put("/", verifyToken, async (req: Request, res: Response) => {
   const token = req.cookies.auth_token;
-    const decodedToken: any = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
-    const userId = decodedToken.userId;
-    const updatedUser: UserType = req.body;
+  const decodedToken: any = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
+  const userId = decodedToken.userId;
+  const updatedUser: UserType = req.body;
 
-    try {
-      const existingUser = await User.findOneAndUpdate(
-        { _id: userId }, updatedUser)
-      if (!existingUser) {
-        return res.status(404).json({ message: "User not found" });
-      }
-      return res.status(200).json(existingUser);
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ message: "Something went wrong" });
+  try {
+    const existingUser = await User.findOneAndUpdate(
+      { _id: userId }, updatedUser)
+    if (!existingUser) {
+      return res.status(404).json({ message: "User not found" });
     }
-  });
+    return res.status(200).json(existingUser);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+router.post('/forgotPassword', async(req: Request, res: Response) => {
+  try {
+    const { email } = req.body;
+
+    if (!email) return res.status(422).json({ message: "User email is required to proceed with resetting the password" });
+
+    const user: any = await User.findOne({ email: email });
+
+    if (!user) return res.status(404).json({ message: "User email could not be found" });
+
+    const { otp, expiration } = await generateOTP(email, "Resetting password");
+
+    return res.status(200).json({ message: `Token will expire on ${expiration.toLocaleString()}`, token: otp })
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+})
+
+router.patch('/resetPassword', async(req: Request, res: Response) => {
+  try {
+    const { token, newPassword } = req.body;
+    const dateNow = new Date()
+
+    if (!token) return res.status(422).json({ message: "Token is required to proceed with resetting the password" });
+    if (!newPassword) return res.status(422).json({ message: "New Password is required to proceed with resetting the password" });
+
+    const arrayOTPT: any = await OTPTransaction.aggregate().match({
+      $expr: {
+        $and: [
+          {
+            $eq: ['$otp', token]
+          },
+          {
+            $gt: ['$expirationDate', { $toDate: dateNow}]
+          }
+        ]
+      }
+    });
+
+    if (arrayOTPT.length > 1) return res.status(404).json({ message: "Not a valid OTP transaction" });
+    else if (arrayOTPT.length === 0) return res.status(404).json({ message: "Could not reset the password, it seems the token is already expired" });
+
+    const OTPT = arrayOTPT[0];
+
+    const user: any = await User.findOne({ email: OTPT.email});
+
+    if (!user) res.status(404).json({ message: "User not found" });
+    
+    await OTPTransaction.updateOne({ _id: OTPT._id }, { status: 'USED'})
+
+    const hashedPass = await generateHash(newPassword);
+    await User.updateOne({ _id: user._id }, { password: hashedPass });
+
+    const newAuthToken = jwt.sign(
+      {
+        userId: user.id,
+        role: user.role
+      },
+      process.env.JWT_SECRET_KEY as string,
+      { expiresIn: "1d" }
+    );
+
+    res.cookie("auth_token", newAuthToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 86400000,
+    })
+
+    return res.status(200).send({ message: "You have changed your password successfully" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+})
 
 export default router;
