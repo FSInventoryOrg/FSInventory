ea12b7c10c9dd09606df1eb77b868ff27a72a8c5
Kenny Clark Quieta <kennyclark.quieta@gmail.com>
2024-07-30 23:13:46 +0800

    add inventory report download

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,8 +1,9 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import * as XLSX from 'xlsx';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export function hexToRgbA(hex: string, alpha: number) {
@@ -21,15 +22,14 @@ export function isWindowScrollable() {
   return bodyScrollHeight > windowHeight || documentScrollHeight > windowHeight;
 }
 
-const isValidUrl = (url:string, base?: string)=> {
+const isValidUrl = (url: string, base?: string) => {
   try {
     new URL(url, base);
     return true;
   } catch (err) {
-
     return false;
   }
-}
+};
 
 export function prependHostIfMissing(path?: string) {
   const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
@@ -38,8 +38,36 @@ export function prependHostIfMissing(path?: string) {
   if (!path) return;
 
   if (isValidUrl(path)) {
-    return path
+    return path;
   } else {
-   return isValidUrl(path, API_BASE_URL)? new URL(path, API_BASE_URL).href : undefined
+    return isValidUrl(path, API_BASE_URL)
+      ? new URL(path, API_BASE_URL).href
+      : undefined;
   }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function exportToExcel(columns: string[], data: any, fileName: string) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const filteredData = data.map((row: any) =>
+    Object.keys(row).reduce((accum: Record<string, string>, key: string) => {
+      if (columns.includes(key)) {
+        accum[key] = row[key];
+      }
+      return accum;
+    }, {})
+  );
+
+  const workbook = XLSX.utils.book_new();
+  const worksheet = XLSX.utils.json_to_sheet(filteredData);
+  const currentDate = new Intl.DateTimeFormat('en-PH', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  }).format(new Date());
+
+  XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+  XLSX.writeFile(workbook, `${fileName}-${currentDate}.xlsx`, {
+    compression: true,
+  });
 }
