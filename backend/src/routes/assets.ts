57d12db0e1d46989a10d8b8c6eda49bdde5a5fae
Alpha <faipacada@gmail.com>
2024-09-12 01:34:32 +0800

    EditAsset dialog to handle editing of software assets

--- backend/src/routes/assets.ts
+++ backend/src/routes/assets.ts
@@ -427,79 +427,75 @@ router.put('/:code', [
       delete data['code'];
 
       let updatedAsset;
-      if (data.type === 'Hardware') {
-        let depHist = existingAsset['deploymentHistory']
-        if (!Array.isArray(depHist)) depHist = [];
-        else depHist = depHist.reverse();
+      let depHist = existingAsset['deploymentHistory']
+      if (!Array.isArray(depHist)) depHist = [];
+      else depHist = depHist.reverse();
 
-        const recoveryProcess = (assignee :string, recoveryDate?: Date, deploymentDate?: Date) => {
-          if(assignee) {
-            const findIndex = depHist.findIndex((f: any) => { return f['assignee'] === assignee })
-            const recovery = {
-              assignee,
-              recoveryDate: recoveryDate ?? new Date()
-            }
-            // Update recovery fields
-            data.recoveredFrom = recovery.assignee
-            data.recoveryDate = recovery.recoveryDate
-            
-            if (findIndex == 0) depHist[findIndex] = { ...depHist[findIndex].toObject(), ...recovery }
-            else {
-              depHist.unshift({...recovery,  deploymentDate: deploymentDate ?? null})
-            }
+      const recoveryProcess = (assignee :string, recoveryDate?: Date, deploymentDate?: Date) => {
+        if(assignee) {
+          const findIndex = depHist.findIndex((f: any) => { return f['assignee'] === assignee })
+          const recovery = {
+            assignee,
+            recoveryDate: recoveryDate ?? new Date()
+          }
+          // Update recovery fields
+          data.recoveredFrom = recovery.assignee
+          data.recoveryDate = recovery.recoveryDate
+          
+          if (findIndex == 0) depHist[findIndex] = { ...depHist[findIndex].toObject(), ...recovery }
+          else {
+            depHist.unshift({...recovery,  deploymentDate: deploymentDate ?? null})
           }
         }
-
-        if(data['assignee']) {
-          // If 'Assignee' is filled in, deploy the asset to assignee
-          data['status'] = "Deployed";
-          if(data['assignee'] !== existingAsset['assignee']) {
-            // Recover asset from old assignee and deploy to new assignee
-            recoveryProcess(existingAsset['assignee'], new Date(), existingAsset['deploymentDate']);
-            const deploy = {
-              assignee: data['assignee'],
-              deploymentDate: data['deploymentDate'] ? data['deploymentDate'] : new Date()
-            }
-            depHist.unshift(deploy)
-          }
-        } else if(data['status'] === 'Deployed' && !data['assignee']) {
-          // Recover asset from current assignee if 'Assignee' field is left blank and status is 'Deployed'
-          data['status'] = "IT Storage";
-          if(data['assignee'] !== existingAsset['assignee']) {
-            recoveryProcess(existingAsset['assignee'],  new Date(), existingAsset['deploymentDate'])
-          }
-        }
-
-        const recoveredFrom = data['recoveredFrom']
-        const recoveryDate = data['recoveryDate']
-        if (existingAsset['status'] === 'Deployed' && existingAsset['status'] !== data['status']) {
-          // Recover asset from current assignee if status is changed from 'Deployed'
-            recoveryProcess(existingAsset['assignee'], new Date(), existingAsset['deploymentDate']) 
-        }
-  
-        // If 'Recovered From' is filled in, recover asset from that assignee regardless of asset status
-        if(recoveredFrom && recoveredFrom !== existingAsset['recoveredFrom']) {
-          recoveryProcess(recoveredFrom, recoveryDate)
-        }
-        
-
-        depHist = depHist.reverse();
-
-        if(depHist.length > 0) data['deploymentHistory'] = depHist
-        
-        // Remove undefined date properties and set them to null
-        data.deploymentDate ??= null;
-        data.purchaseDate ??= null;
-        data.recoveryDate ??= null;
-
-        // Update the asset
-        updatedAsset = await Hardware.findOneAndUpdate({ code: code }, data, { new: true });
-
-        await auditAssets();
-      } else {
-        updatedAsset = await Software.findOneAndUpdate({ code: code }, data, { new: true });
       }
 
+      if(data['assignee']) {
+        // If 'Assignee' is filled in, deploy the asset to assignee
+        data['status'] = "Deployed";
+        if(data['assignee'] !== existingAsset['assignee']) {
+          // Recover asset from old assignee and deploy to new assignee
+          recoveryProcess(existingAsset['assignee'], new Date(), existingAsset['deploymentDate']);
+          const deploy = {
+            assignee: data['assignee'],
+            deploymentDate: data['deploymentDate'] ? data['deploymentDate'] : new Date()
+          }
+          depHist.unshift(deploy)
+        }
+      } else if(data['status'] === 'Deployed' && !data['assignee']) {
+        // Recover asset from current assignee if 'Assignee' field is left blank and status is 'Deployed'
+        data['status'] = "IT Storage";
+        if(data['assignee'] !== existingAsset['assignee']) {
+          recoveryProcess(existingAsset['assignee'],  new Date(), existingAsset['deploymentDate'])
+        }
+      }
+
+      const recoveredFrom = data['recoveredFrom']
+      const recoveryDate = data['recoveryDate']
+      if (existingAsset['status'] === 'Deployed' && existingAsset['status'] !== data['status']) {
+        // Recover asset from current assignee if status is changed from 'Deployed'
+          recoveryProcess(existingAsset['assignee'], new Date(), existingAsset['deploymentDate']) 
+      }
+
+      // If 'Recovered From' is filled in, recover asset from that assignee regardless of asset status
+      if(recoveredFrom && recoveredFrom !== existingAsset['recoveredFrom']) {
+        recoveryProcess(recoveredFrom, recoveryDate)
+      }
+      
+
+      depHist = depHist.reverse();
+
+      if(depHist.length > 0) data['deploymentHistory'] = depHist
+      
+      // Remove undefined date properties and set them to null
+      data.deploymentDate ??= null;
+      data.purchaseDate ??= null;
+      data.recoveryDate ??= null;
+
+      // Update the asset
+      updatedAsset = await Asset.findOneAndUpdate({ code: code }, data, { new: true });
+
+      await auditAssets();
+      
       res.status(200).json(updatedAsset);
     } catch (error) {
       console.error('Error updating asset:', error);
