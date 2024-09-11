5237d726db62a4014263f235a8d18eede659f304
Alpha <faipacada@gmail.com>
2024-09-11 21:59:32 +0800

    Refactored the Options Component and moved code to AddOption and EditOption components

--- frontend/src/components/inventory-ui/Options.tsx
+++ frontend/src/components/inventory-ui/Options.tsx
@@ -11,32 +11,8 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover"
 import { ScrollArea } from '../ui/scroll-area';
-import { ChevronLeftIcon, ChevronsUpDownIcon, PencilIcon, XIcon } from 'lucide-react';
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils"
-import { Input } from "@/components/ui/input";
-import { Label } from '@/components/ui/label';
-import { Checkbox } from "@/components/ui/checkbox"
-import { Separator } from '@/components/ui/separator';
-import { Spinner } from '../Spinner';
-import TagSelect from './TagSelect'
-import ColorSelect from './ColorSelect'
-import DeletePropertyDialog from './DeletePropertyDialog'
-
-export type ColorOption = {
-  value: string;
-  color?: string;
-}
-
-export type TagOption = {
-  value: string;
-  properties?: string[];
-}
-
-type OptionType = {
-  property: string;
-  value: string | ColorOption | TagOption;
-}
+import AddOption from './AddOption';
+import EditOption from './EditOption';
 
 interface OptionsProps {
   property: string;
@@ -61,56 +37,32 @@ const Options = ({ property, colorSelect=false, tagSelect=false, field, classNam
   const [open, setOpen] = React.useState(false)
   const [openDeleteOptionDialog, setOpenDeleteOptionDialog] = React.useState(false)
   const { showToast } = useAppContext();
-  const [newOption, setNewOption] = React.useState<OptionType>({ property: property, value: '' });
+  const [newOption, setNewOption] = React.useState<OptionType>({
+    property: property,
+    value: '',
+  });
   const [optionToEdit, setOptionToEdit] = React.useState<string>('');
   const [isCreating, setIsCreating] = React.useState<boolean>(false);
   const [isEditing, setIsEditing] = React.useState<boolean>(false);
 
-  const { data: optionValues } = useQuery<string[] | ColorOption[] | TagOption[]>({ 
+  const { data: optionValues } = useQuery<
+    string[] | ColorOption[] | TagOption[]
+  >({
     queryKey: ['fetchOptionValues', property], 
     queryFn: () => imsService.fetchOptionValues(property),
     enabled: open,
-  })
+  });
 
   const [filterValue, setFilterValue] = React.useState('');
   const [filteredData, setFilteredData] = React.useState<ColorOption[]>([]);
 
-  const handleTagSelect = (tags: string[]) => {
-    if (newOption) {
-      if (typeof newOption.value === 'string') {
-        const newTagOption: TagOption = {
-          value: newOption.value,
-          properties: tags
-        };
-        setNewOption({ ...newOption, value: newTagOption });
-      } else if (typeof newOption.value === 'object' && 'value' in newOption.value) {
-        const newValue = { ...newOption.value, properties: tags };
-        setNewOption({ ...newOption, value: newValue });
-      }
+  const getOptionValue = (option: ColorOption | TagOption | string) => {
+    if (typeof option === 'string') {
+      return option;
+    } else if (typeof option === 'object' && option.value) {
+      return option.value;
     }
-  };
-
-  const handleColorSelect = (color: string) => {
-    if (newOption) {
-      if (typeof newOption.value === 'string') {
-        const newColorOption: ColorOption = {
-          value: newOption.value,
-          color: color
-        };
-        setNewOption({ ...newOption, value: newColorOption });
-      } else {
-        if (color === '') {
-          const { value } = newOption.value;
-          setNewOption({ ...newOption, value: value });
-        } else {
-          const updatedColorOption: ColorOption = {
-            ...newOption.value,
-            color: color
-          };
-          setNewOption({ ...newOption, value: updatedColorOption });
-        }
-      }
-    }
+    return '';
   };
 
   const { mutate: addOptionValue, isPending: isAddPending } = useMutation({
@@ -144,16 +96,49 @@ const Options = ({ property, colorSelect=false, tagSelect=false, field, classNam
       mutationFn: imsService.updateAssetsByProperty,
     });
 
-  const handleDelete = () => {
-    if (optionValues) {
-      const indexOfValueToDelete = optionValues.findIndex(option => {
+  const findIndexInOptions = (optionVal: string) => {
+    if (!optionValues) return -1;
+    return optionValues.findIndex((option) => {
         if (typeof option === 'string') {
-          return option === optionToEdit;
+        return option === optionVal;
         } else if (typeof option === 'object' && option.value) {
-          return option.value === optionToEdit;
-        }
-        return false;
-      });
+        return option.value === optionVal;
+      }
+      return false; // Filter out undefined or other non-matching types
+    });
+  };
+
+  const handleAddOption = (option: OptionType) => {
+    addOptionValue({ ...option, type });
+  };
+  const handleUpdateOption = (updatedOption: OptionType) => {
+    const indexOfValueToEdit = findIndexInOptions(optionToEdit);
+    updateOptionValue({
+      property: updatedOption.property,
+      value: updatedOption.value,
+      index: indexOfValueToEdit,
+    });
+    updateAssetsByProperty({
+      property: updatedOption.property,
+      value: optionToEdit,
+      newValue:
+        typeof updatedOption.value === 'object'
+          ? updatedOption.value.value
+          : updatedOption.value,
+    });
+
+    if (indexOfValueToEdit === findIndexInOptions(field.value)) {
+      field.onChange(
+        typeof updatedOption.value === 'object'
+          ? updatedOption.value.value
+          : updatedOption.value
+      );
+    }
+  };
+
+  const handleDeleteOption = (optionToDelete: string) => {
+    if (optionValues) {
+      const indexOfValueToDelete = findIndexInOptions(optionToDelete);
 
       if (indexOfValueToDelete !== -1) {
         const valueToDelete = optionValues[indexOfValueToDelete];
@@ -172,12 +157,20 @@ const Options = ({ property, colorSelect=false, tagSelect=false, field, classNam
         }
       }
     }
-    setOpen(false)
+    setOpen(false);
     setTimeout(() => {
       setOpen(true);
     }, 100);
-    showToast({ message: `Option deleted successfully!`, type: "SUCCESS" });
-  }
+    showToast({ message: `Option deleted successfully!`, type: 'SUCCESS' });
+  };
+
+  const reset = () => {
+    setFilterValue('');
+    setTimeout(() => {
+      setFilteredData([]);
+    }, 100);
+    setNewOption({ property: property, value: '' });
+  };
 
   React.useEffect(() => {
     if (open) {
@@ -341,156 +334,43 @@ const Options = ({ property, colorSelect=false, tagSelect=false, field, classNam
             Create a new {format(property)}
           </Button>
         </div>
-        <div id='create panel' className={`flex flex-col gap-3 ${isCreating ? '': 'hidden'}`}>
-          <div className='items-center flex flex-row'>
-            <Button
-              type='button'
-              size='icon' 
-              variant='ghost' 
-              className='absolute h-10 w-10'
-              onClick={() => {
-                setIsCreating(!isCreating)
-                setNewOption({ property: property, value: '' })
-              }}
-            >
-              <ChevronLeftIcon />
-            </Button>
-            <h1 className='w-full flex justify-center items-center font-semibold text-sm h-10'>Create {format(property)}</h1>
-          </div>
-          <Label>Value</Label>
-          <Input
-            value={
-              getOptionValue(newOption.value)
-            }
-            type="input"
-            className="focus-visible:ring-0 focus-visible:ring-popover"
-            onChange={(e) => {
-              const newValue = e.target.value;
-              if (typeof newOption === 'object') {
-                setNewOption(prevOption => {
-                  const updatedValue = typeof prevOption.value === 'object'
-                      ? { ...prevOption.value, value: newValue }
-                      : newValue;
-                  return { ...prevOption, value: updatedValue };
-                });
-              } else {
-                setNewOption({ property: property, value: newValue });
-              }
-            }}
-            onKeyDown={(e) => {
-              if (e.key === 'Enter') {
-                e.preventDefault();
-                setOpen(!open)
-              }
-            }}
-          />
-          {colorSelect && <ColorSelect onColorSelect={handleColorSelect} reset={isEditing || isCreating} />}
-          {tagSelect && <TagSelect onTagSelect={handleTagSelect} reset={isEditing || isCreating} />}
-          <Separator className='my-1' />
-          <Button
-            className='gap-2'
-            disabled={isAddPending}
-            type='button'
-            onClick={() => {
-              if (newOption) {
-                addOptionValue(newOption)
-              }
-            }}
+        {isCreating && (
+          <div
+            id='create panel'
+            className={`flex flex-col gap-3 ${isCreating ? '' : 'hidden'}`}
           >
-            {isAddPending ? <Spinner size={18}/> : null }
-            Create
-          </Button>
-        </div>
-        <div id='edit panel' className={`flex flex-col gap-3 ${isEditing ? '': 'hidden'}`}>
-          <div className='items-center flex flex-row'>
-            <Button
-              type='button'
-              size='icon' 
-              variant='ghost' 
-              className='absolute h-10 w-10'
-              onClick={() => {
-                setIsEditing(!isEditing)
-                setNewOption({ property: property , value: '' })
-              }}
-            >
-              <ChevronLeftIcon />
-            </Button>
-            <h1 className='w-full flex justify-center items-center font-semibold text-sm h-10'>Edit {format(property)}</h1>
-          </div>
-          <Label>Value</Label>
-          <Input
-            value={
-             getOptionValue(newOption.value)
-            }
-            type="input"
-            className="focus-visible:ring-0 focus-visible:ring-popover"
-            onChange={(e) => {
-              setNewOption({ property: property, value: e.target.value});
-            }}
-            onKeyDown={(e) => {
-              if (e.key === 'Enter') {
-                e.preventDefault();
-                setOpen(!open)
-              }
-            }}
-          />
-          {colorSelect && <ColorSelect onColorSelect={handleColorSelect} property={property} option={optionToEdit} />}
-          {tagSelect && <TagSelect onTagSelect={handleTagSelect} property={property} option={optionToEdit} />}
-          <Separator className='my-1' />
-          <div className='flex justify-between'>
-            <Button
-              className='gap-2'
-              disabled={isOptionEditPending || isAssetEditPending}
-              type='button'
-              onClick={() => {
-                if (newOption && optionValues && optionToEdit) {
-                  const indexOfValueToEdit = optionValues.findIndex(option => {
-                    if (typeof option === 'string') {
-                        return option === optionToEdit;
-                    } else if (typeof option === 'object' && option.value) {
-                        return option.value === optionToEdit;
-                      }
-                      return false; // Filter out undefined or other non-matching types
-                  });
-                  // console.log(indexOfValueToEdit)
-                  // console.log(newOption.property)
-                  // console.log(newOption.value)
-                  updateOptionValue({ property: newOption.property, value: newOption.value, index: indexOfValueToEdit });
-                  updateAssetsByProperty({ property: newOption.property, value: optionToEdit, newValue: typeof newOption.value === 'object' ? newOption.value.value : newOption.value });
-                  if (indexOfValueToEdit === optionValues.findIndex(option => {
-                    if (typeof option === 'string') {
-                        return option === field.value;
-                    } else if (typeof option === 'object' && option.value) {
-                        return option.value === field.value;
-                      }
-                      return false; // Filter out undefined or other non-matching types
-                  })) {
-                    field.onChange(typeof newOption.value === 'object' ? newOption.value.value : newOption.value);
-                  }
-                }
-              }}
-            >
-              {(isOptionEditPending || isAssetEditPending) ? <Spinner size={18}/> : null }
-              Save
-            </Button>
-            <Button className="inline-flex items-center justify-center whitespace-nowrap rounded-md 
-              text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none 
-              focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none 
-              disabled:opacity-50 bg-destructive text-destructive-foreground hover:bg-destructive/90 h-10 px-4 py-2"
-              onClick={(event)=>{
-                event.preventDefault();
-                setOpenDeleteOptionDialog(true)}}>
-              Delete
-            </Button>
-            <DeletePropertyDialog
-              open={openDeleteOptionDialog}
-              setOpen={setOpenDeleteOptionDialog}
+            <AddOption
+              defaultOption={newOption}
               property={property}
-              value={optionToEdit}
-              onDelete={handleDelete}
+              colorSelect={colorSelect}
+              tagSelect={tagSelect}
+              onEnterPressed={() => setOpen(!open)}
+              onCancel={() => setIsCreating((prev) => !prev)}
+              onSave={handleAddOption}
+              isCreating={isCreating}
+              isAddPending={isAddPending}
             />
           </div>
-        </div>
+        )}
+
+        {isEditing && (
+          <div
+            id='edit panel'
+            className={`flex flex-col gap-3 ${isEditing ? '' : 'hidden'}`}
+          >
+            <EditOption
+              option={newOption}
+              property={property}
+              colorSelect={colorSelect}
+              tagSelect={tagSelect}
+              onEnterPressed={() => setOpen(!open)}
+              onCancel={() => setIsEditing((prev) => !prev)}
+              onDelete={handleDeleteOption}
+              onUpdate={handleUpdateOption}
+              isUpdatePending={isOptionEditPending || isAssetEditPending}
+            />
+          </div>
+        )}
       </PopoverContent>
     </Popover>
   )
