77439e4f730b7354d6a8d1d79f14ea41fcc03e9b
Yul Sullano <ysullano@fullscale.io>
2024-09-20 21:51:35 +0800

    Create BackupValidationModal

---
+++ frontend/src/components/setttings-ui/load-backup/BackupValidationModal.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { Spinner } from '@/components/Spinner';
+import { ValidationResult } from './LoadBackupForm';
+
+interface BackupValidationModalProps {
+  result: ValidationResult
+}
+
+export const BackupValidationModal: React.FC<BackupValidationModalProps> = ({ result }) => {
+
+  return(<></>)
+}
