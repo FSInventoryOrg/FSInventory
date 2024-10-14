819a605609138bf854371dd4b28695bb114cadfb
Alpha <faipacada@gmail.com>
2024-10-14 14:02:28 +0800

    Updated schema for request form

--- frontend/src/schemas/RequestFormSchema.ts
+++ frontend/src/schemas/RequestFormSchema.ts
@@ -3,22 +3,22 @@ export const RequestFormSchema = z.object({
     requestType: z.enum(['Report an Issue', 'Request a New Asset'], {
       required_error: 'Request Type is required',
     }),
-    fullName: z.string().min(1,'Full name is required'),
-    manager: z.string().min(1,'Manager is required'),
+    fullName: z.string().trim().min(1, 'Full name is required'),
+    manager: z.string().trim().min(1, 'Manager is required'),
     contactInfo: z.string().min(1,'Contact information is required'),
   });
 
 
 export const ReportIssueSchema = RequestFormSchema.extend({
-    issueCategory: z.string().min(1, 'Issue category is required'),
+    issueCategory: z.string().trim().min(1, 'Issue category is required'),
     assetAffected: z.string().optional(),
-    problemDescription: z.string().min(1, 'Detailed description is required'),
+    problemDescription: z.string().trim().min(1, 'Detailed description is required'),
     supportingFiles: z.any().optional()
 })
 
 export const RequestNewAssetSchema = RequestFormSchema.extend({
-    assetType: z.string().optional(),
-    assetSpecification: z.string().optional(),
-    requestJustification: z.string().optional(),
-    requestDate: z.string().optional()
+    assetType: z.string().min(1, 'Asset type is required'),
+    assetSpecification: z.string().trim().min(1, 'Asset specifications or model is required'),
+    requestJustification: z.string().trim().min(1, 'Justification for request is required'),
+    requestDate: z.date().nullable().optional(),
 })
