9499dc63b3e1063db946cd13f3376f8bbdfe597b
Alpha <faipacada@gmail.com>
2024-10-17 18:42:09 +0800

    Updated active color of tabs in Add Asset and Edit Asset

--- frontend/src/components/inventory-ui/EditAsset.tsx
+++ frontend/src/components/inventory-ui/EditAsset.tsx
@@ -181,8 +181,8 @@ const onSubmit = (data: any) => {
           />
           <Tabs defaultValue='Hardware' value={assetData.type} className="w-full">
             <TabsList className="grid w-full grid-cols-2">
-              <TabsTrigger value="Hardware" onClick={() => handleTabChange("Hardware")}>Hardware</TabsTrigger>
-              <TabsTrigger value="Software" onClick={() => handleTabChange("Software")} disabled>Software</TabsTrigger>
+              <TabsTrigger className='data-[state=active]:bg-tab' value="Hardware" onClick={() => handleTabChange("Hardware")}>Hardware</TabsTrigger>
+              <TabsTrigger className='data-[state=active]:bg-tab' value="Software" onClick={() => handleTabChange("Software")} disabled>Software</TabsTrigger>
             </TabsList>
             <div className="">
               <TabsContent tabIndex={-1} value="Hardware" className="pb-4 px-3">
