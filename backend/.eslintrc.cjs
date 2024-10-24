c870cefedec367e41d68df820f363b1eb57cd415
hansel-cmd <hanselcesa@gmail.com>
2024-10-24 15:41:26 +0800

    feat(eslint): [NO-TICKET] add eslintrc config for nodejs

--- backend/.eslintrc.cjs
+++ backend/.eslintrc.cjs
@@ -2,26 +2,30 @@ module.exports = {
   env: {
     node: true,
     commonjs: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
-    'eslint:recommended',
-    'prettier'
+    "eslint:recommended",
+    "prettier",
+    "plugin:@typescript-eslint/recommended", // Enable TypeScript linting rules
   ],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'script'
+    sourceType: "module",
   },
   rules: {
-    'no-console': 'off',
-    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    'no-multi-spaces': ['error'],
-    'prefer-const': 'error',
-    'eqeqeq': 'error',
-    'no-var': 'error',
-    'semi': ['error', 'always'],
-    'quotes': ['error', 'single'],
-    'indent': ['error', 2],
-    'no-mixed-spaces-and-tabs': 'error'
-  }
+    "no-console": "off",
+    "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "no-multi-spaces": ["error"],
+    "prefer-const": "error",
+    eqeqeq: "error",
+    "no-var": "error",
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+    indent: ["error", 2],
+    "no-mixed-spaces-and-tabs": "error",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unused-vars": ["warn"],
+  },
 };
