9780ddd1e75195a83d79abf0fb5819b19defc61a
hansel-cmd <hanselcesa@gmail.com>
2024-10-24 12:53:00 +0800

    feat(improvement): [NO-TICKET] test commit

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -5,41 +5,42 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:prettier/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'prettier',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: ['react', '@typescript-eslint', 'prettier'],
+  plugins: ["react", "@typescript-eslint", "prettier"],
   rules: {
-    indent: ['error', 2],
-    'react/jsx-indent': ['error', 2],
-    'react/jsx-indent-props': ['error', 2],
-    'no-multi-spaces': ['error'],
-    quotes: ['error', 'double'],
-    'prettier/prettier': [
-      'error',
+    indent: ["error", 2],
+    "react/jsx-indent": ["error", 2],
+    "react/jsx-indent-props": ["error", 2],
+    "no-multi-spaces": ["error"],
+    quotes: ["error", "double"],
+    "prettier/prettier": [
+      "error",
       {
         singleQuote: false,
         tabWidth: 2,
+        endOfLine: "auto",
       },
     ],
-    'react/react-in-jsx-scope': 'off'
+    "react/react-in-jsx-scope": "off",
   },
   settings: {
     react: {
-      version: 'detect',
+      version: "detect",
     },
   },
 };
