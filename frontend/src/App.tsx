3013b361c25a4f1a3215bd878f14245538324cbf
hansel-cmd <hanselcesa@gmail.com>
2024-10-28 16:27:04 +0800

    feat(updated-user-profile): remove profile page, and its components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,6 @@ import Dashboard from "./pages/Dashboard";
 import Inventory from "./pages/Inventory";
 import Loading from "./pages/Loading";
 import Tracker from "./pages/Tracker";
-import Profile from "./pages/Profile";
 import Settings from "./pages/Settings";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
@@ -33,7 +32,6 @@ const App = () => {
                   <Route path="/inventory" element={<Layout><Inventory /></Layout>} />
                   <Route path="/tracker" element={<Layout><Tracker /></Layout>} />
                   <Route path="/tracker/:employeeCode" element={<Layout><Tracker /></Layout>} />
-                  <Route path="/profile" element={<Layout><Profile/></Layout>}/>
                   <Route path="/settings/*" element={<Layout><Settings /></Layout>}/>
                   {/* <Route path="/requests" element={<Layout>Requests</Layout>} /> */}
                 </>
