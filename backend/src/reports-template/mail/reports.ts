679984d67842a925752e3ec273507c5c4b214724
Yul Sullano <ysullano@fullscale.ph>
2024-10-31 20:49:37 +0800

    Fix linting errors; change templatePath to working path

--- backend/src/reports-template/mail/reports.ts
+++ backend/src/reports-template/mail/reports.ts
@@ -1,29 +1,29 @@
-import Handlebars from 'handlebars';
-import Asset from '../../models/asset.schema';
-import AssetCounter from '../../models/asset-counter.schema';
-import Option from '../../models/options.schema';
-import AutoMail from '../../models/automail.schema';
-import { getFile } from '../../utils/common';
+import Handlebars from "handlebars";
+import Asset from "../../models/asset.schema";
+import AssetCounter from "../../models/asset-counter.schema";
+import Option from "../../models/options.schema";
+import AutoMail from "../../models/automail.schema";
+import { getFile } from "../../utils/common";
 
 export const inventoryReportHtml = async (mailMeta?: any) => {
-  const templatePath = `src/reports-template/mail/report.hbs`;
+  const templatePath = "backend/src/reports-template/mail/report.hbs";
   const templateContent = await getFile(templatePath);
   const template = Handlebars.compile(templateContent?.toString());
 
-  let option = await Option.findOne();
-  const statuses = option?.get('status').map((status) => status.value) || [];
+  const option = await Option.findOne();
+  const statuses = option?.get("status").map((status) => status.value) || [];
   const categories =
-    option?.get('category').map((category) => category.value) || [];
+    option?.get("category").map((category) => category.value) || [];
 
   const hardwareAssets: any = {};
   for (const category of categories) {
     const data = await Asset.aggregate()
       .match({
-        type: 'Hardware',
+        type: "Hardware",
         category: category,
       })
       .group({
-        _id: '$status',
+        _id: "$status",
         count: {
           $count: {},
         },
@@ -41,18 +41,18 @@ export const inventoryReportHtml = async (mailMeta?: any) => {
         hardwareAssets[category][status] = 0;
       sum += hardwareAssets[category][status];
     }
-    hardwareAssets[category]['Total'] = sum;
+    hardwareAssets[category]["Total"] = sum;
   }
 
   // Total per Status
-  for (const status of [...statuses, 'Total']) {
-    if (!hardwareAssets['Total per Status'])
-      hardwareAssets['Total per Status'] = {};
+  for (const status of [...statuses, "Total"]) {
+    if (!hardwareAssets["Total per Status"])
+      hardwareAssets["Total per Status"] = {};
     let sum = 0;
     for (const category of categories) {
       sum += hardwareAssets[category][status];
     }
-    hardwareAssets['Total per Status'][status] = sum;
+    hardwareAssets["Total per Status"][status] = sum;
   }
 
   const autoMailSettings = await AutoMail.findOne();
@@ -81,17 +81,17 @@ export const inventoryReportHtml = async (mailMeta?: any) => {
 
   const assetCounters = (
     await AssetCounter.find({
-      status: 'Depleting',
+      status: "Depleting",
     })
   ).map((asset) => asset.toObject());
 
-  let data = {
+  const data = {
     statuses,
     categories,
     hardwareAssets,
     latestAssets,
     assetCounters,
-    contact: mailMeta?.contact || 'admin@fullscale.ph',
+    contact: mailMeta?.contact || "admin@fullscale.ph",
   };
 
   const htmlMessage = template(data);
