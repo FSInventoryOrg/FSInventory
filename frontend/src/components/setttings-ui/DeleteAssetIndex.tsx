727aa71c03ce7555d5aa1a5743e6ccf3d681d445
Alpha <faipacada@gmail.com>
2024-11-04 14:59:05 +0800

    Removed delete mutation since this action is moved to backend

--- frontend/src/components/setttings-ui/DeleteAssetIndex.tsx
+++ frontend/src/components/setttings-ui/DeleteAssetIndex.tsx
@@ -3,9 +3,7 @@ import { Button } from "../ui/button";
 import DeletePropertyDialog from "../inventory-ui/DeletePropertyDialog";
 import { useState } from "react";
 import { AssetCounterType } from "@/types/asset";
-import { useMutation } from '@tanstack/react-query';
-import * as imsService from "@/ims-service";
-import { useAppContext } from '@/hooks/useAppContext';
+import { useAppContext } from "@/hooks/useAppContext";
 
 interface DeleteProps {
   data: AssetCounterType;
@@ -14,37 +12,38 @@ interface DeleteProps {
 const DeleteAssetIndex = ({ data }: DeleteProps) => {
   const [openDialog, setOpenDialog] = useState(false);
   const { showToast } = useAppContext();
-  
+
   const onDeleteSuccess = () => {
     showToast({
-      message: 'Asset counter and category deleted successfully!',
-      type: 'SUCCESS',
+      message: "Asset counter and category deleted successfully!",
+      type: "SUCCESS",
     });
-  }
+  };
 
-  const { mutate: deleteAssetCounter, isPending } = useMutation({
-    mutationFn: imsService.deleteAssetCounter,
-    onSuccess: async () => {
-      onDeleteSuccess();
-    },
-    onError: (error: Error) => {
-      showToast({ message: error.message, type: "ERROR"});
-    },
-  });
+  // const { mutate: deleteAssetCounter, isPending } = useMutation({
+  //   mutationFn: imsService.deleteAssetCounter,
+  //   onSuccess: async () => {
+  //     onDeleteSuccess();
+  //   },
+  //   onError: (error: Error) => {
+  //     showToast({ message: error.message, type: "ERROR" });
+  //   },
+  // });
 
   const handleDelete = () => {
-    if (!data._id) { 
-      // Deleted category has no existing asset counter. Occurs for new categories not yet
-      // configured with asset index or counter
-      return onDeleteSuccess()
-    }
-    deleteAssetCounter(data.prefixCode as string);
-  }
- 
+    // if (!data._id) {
+    //   // Deleted category has no existing asset counter. Occurs for new categories not yet
+    //   // configured with asset index or counter
+    //   return onDeleteSuccess()
+    // }
+    // deleteAssetCounter(data.prefixCode as string);
+    onDeleteSuccess();
+  };
+
   return (
     <>
       <Button
-        disabled={isPending}
+        // disabled={isPending}
         variant="ghost"
         size="icon"
         className="text-destructive"
