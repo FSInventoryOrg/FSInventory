5d54dc6824e2e26015ff55771b4dd568819cfb03
Fritz Gerald Dumdum <fritz.dumdum@redeem.xyz>
2024-11-06 12:03:02 +0800

    improvement: isolate compose file

--- backend/restore.sh
+++ backend/restore.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # TODO: execute this shell script once mongodb connection is instantiated, but for now, manually run below command inside mongodb container to seed data
-mongorestore --uri="mongodb://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@mongodb:27017/inventory?authSource=${MONGODB_AUTH_SOURCE}" $DUMP_DIRECTORY
+
+if [ "$NODE_ENV" = "development" ]; then
+    mongorestore --uri="mongodb://mongodb:27017/${MONGODB_NAME}" $DUMP_DIRECTORY
+else
+    mongorestore --uri="mongodb://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@mongodb:27017/${MONGODB_NAME}?authSource=${MONGODB_AUTH_SOURCE}" $DUMP_DIRECTORY
+fi
