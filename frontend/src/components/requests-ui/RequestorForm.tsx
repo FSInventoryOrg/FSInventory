81064bc671af9e9026d36f60791d358ae03c5707
Alpha <faipacada@gmail.com>
2024-11-09 03:37:32 +0800

    Replaced manager field input component with a dropdown to select manager

--- frontend/src/components/requests-ui/RequestorForm.tsx
+++ frontend/src/components/requests-ui/RequestorForm.tsx
@@ -7,9 +7,17 @@ import {
   FormLabel,
   FormMessage,
 } from "../ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 
 const RequestorForm = () => {
   const { control } = useFormContext();
+  const managers = ["Rodney Fernandez", "John Doe"]; // NOTE: to be replaced with actual data
   return (
     <div className="flex flex-col gap-2">
       {/* Autopopulate if logged in */}
@@ -37,7 +45,26 @@ const RequestorForm = () => {
               Manager
             </FormLabel>
             <FormControl>
-              <Input {...field} />
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <SelectTrigger className="w-full">
+                  <SelectValue
+                    placeholder={
+                      <p className="font-normal text-muted-foreground">
+                        Select manager
+                      </p>
+                    }
+                  >
+                    {field.value}
+                  </SelectValue>
+                </SelectTrigger>
+                <SelectContent>
+                  {managers.map((manager) => (
+                    <SelectItem key={manager} value={manager}>
+                      {manager}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
             </FormControl>
             <FormMessage />
           </FormItem>
