9b7e513e5a53416fc9de0860a78d978e1ab5c01d
Alpha <faipacada@gmail.com>
2024-11-12 01:17:06 +0800

    Removed commented code and updated RequestForm Schema

--- frontend/src/schemas/RequestFormSchema.ts
+++ frontend/src/schemas/RequestFormSchema.ts
@@ -7,6 +7,7 @@ export const RequestorFormSchema = z.object({
 });
 
 export const ReportIssueSchema = z.object({
+  requestType: z.literal("Report an Issue"),
   issueCategory: z.string().trim().min(1, "Issue category is required"),
   assetAffected: z.string().optional(),
   problemDescription: z
@@ -17,6 +18,7 @@ export const ReportIssueSchema = z.object({
 });
 
 export const RequestNewAssetSchema = z.object({
+  requestType: z.literal("Request a New Asset"),
   assetType: z.string().min(1, "Asset type is required"),
   assetSpecification: z
     .string()
@@ -28,68 +30,23 @@ export const RequestNewAssetSchema = z.object({
     .min(1, "Justification for request is required"),
   requestDate: z.date().nullable().optional(),
 });
-
+const REQUEST_TYPES = ["Report an Issue", "Request a New Asset"] as const;
 export const RequestFormSchema = z
   .object({
-    requestType: z.enum(["Report an Issue", "Request a New Asset"], {
+    requestType: z.enum(REQUEST_TYPES, {
       required_error: "Request Type is required",
     }),
   })
   .and(RequestorFormSchema)
   .and(
-    z.union([ReportIssueSchema, RequestNewAssetSchema])
-    // z.union([
-    //   z
-    //     .object({ requestType: z.literal("Report an Issue") })
-    //     .and(ReportIssueSchema),
-    //   z
-    //     .object({ requestType: z.literal("Request a New Asset") })
-    //     .and(RequestNewAssetSchema),
-    // ])
+    z.discriminatedUnion("requestType", [
+      ReportIssueSchema,
+      RequestNewAssetSchema,
+    ])
   );
 
 export type ReportIssueFormData = z.infer<typeof ReportIssueSchema>;
 export type RequestAssetFormData = z.infer<typeof RequestNewAssetSchema>;
 export type RequestorFormData = z.infer<typeof RequestorFormSchema>;
 export type RequestFormData = z.infer<typeof RequestFormSchema>;
-
-// import { z } from "zod";
-
-// export const RequestorFormSchema = z.object({
-//   fullName: z.string().trim().min(1, "Full name is required"),
-//   manager: z.string().trim().min(1, "Manager is required"),
-//   contactInfo: z.string().min(1, "Contact information is required"),
-// });
-
-// export const RequestFormSchema = RequestorFormSchema.extend({
-//   requestType: z.enum(["Report an Issue", "Request a New Asset"], {
-//     required_error: "Request Type is required",
-//   }),
-// });
-
-// export const ReportIssueSchema = RequestFormSchema.extend({
-//   issueCategory: z.string().trim().min(1, "Issue category is required"),
-//   assetAffected: z.string().optional(),
-//   problemDescription: z
-//     .string()
-//     .trim()
-//     .min(1, "Detailed description is required"),
-//   supportingFiles: z.array(z.instanceof(File)).optional(),
-// });
-
-// export const RequestNewAssetSchema = RequestFormSchema.extend({
-//   assetType: z.string().min(1, "Asset type is required"),
-//   assetSpecification: z
-//     .string()
-//     .trim()
-//     .min(1, "Asset specifications or model is required"),
-//   requestJustification: z
-//     .string()
-//     .trim()
-//     .min(1, "Justification for request is required"),
-//   requestDate: z.date().nullable().optional(),
-// });
-
-// export type ReportIssueFormData = z.infer<typeof ReportIssueSchema>;
-// export type RequestAssetFormData = z.infer<typeof RequestNewAssetSchema>;
-// export type RequestorFormData = z.infer<typeof RequestorFormSchema>;
+export type RequestType = (typeof REQUEST_TYPES)[number];
