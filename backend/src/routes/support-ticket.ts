c99c6835d6b368e05bb0b6c7b5b3a522bd568ae1
hansel-cmd <hanselcesa@gmail.com>
2024-11-12 15:20:57 +0800

    feat(support-ticket-backend): add sample usage for the models in the router

--- backend/src/routes/support-ticket.ts
+++ backend/src/routes/support-ticket.ts
@@ -1,12 +1,22 @@
 import express, { Request, Response } from "express";
 import { validationResult } from "express-validator";
+import {
+  SupportTicket,
+  AssetRequestTicket,
+  TicketStatus,
+  TicketType,
+  AssetType,
+  IAssetRequestTicket,
+} from "../models/support-ticket.schema";
 
 const router = express.Router();
 
 router.get("/", [], async (req: Request, res: Response) => {
+  const tickets = await SupportTicket.find();
   return res.status(200).json({
     message: "Hello World",
     status: "OK",
+    items: tickets,
   });
 });
 
@@ -15,10 +25,26 @@ router.post("/", [], async (req: Request, res: Response) => {
   if (!errors.isEmpty()) {
     return res.status(400).json({ message: errors.array() });
   }
-  
+
+  const _supportTicket = new AssetRequestTicket<IAssetRequestTicket>({
+    type: TicketType.AssetRequest,
+    assetSpecsModel: "Macbook M3",
+    assetType: AssetType.Hardware,
+    employeeEmail: "katarinayu@gmail.com",
+    employeeName: "Katarina Yu",
+    createdBy: "Katarina Yu",
+    updatedBy: "Katarina Yu",
+    justification: "I need a Macbook, pls",
+    managerEmail: "manager@gmail.com",
+    managerName: "My Manager",
+    status: TicketStatus.PendingManager,
+  });
+  const saved = await _supportTicket.save();
+
   return res.status(200).json({
     json: "POST Success",
     status: "OK",
+    item: saved,
   });
 });
 
