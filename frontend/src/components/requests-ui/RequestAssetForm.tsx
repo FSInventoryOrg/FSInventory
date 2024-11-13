99acaecd165a5758d5826007cc7aa3748d989d27
Alpha <faipacada@gmail.com>
2024-11-13 14:06:34 +0800

    Updated styles and captions in RequestAssetForm

--- frontend/src/components/requests-ui/RequestAssetForm.tsx
+++ frontend/src/components/requests-ui/RequestAssetForm.tsx
@@ -18,6 +18,7 @@ import { cn } from "@/lib/utils";
 import { useFormContext } from "react-hook-form";
 import {
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormMessage,
@@ -29,6 +30,28 @@ const RequestAssetForm = () => {
   const [openRequestedDate, setOpenRequestedDate] = useState(false);
 
   const requestAssetForm = useFormContext();
+  const assetTypes = [
+    {
+      value: "hardware",
+      label: "Hardware Asset",
+      caption: "Test device, mouse, laptop, monitor, etc.",
+    },
+    {
+      value: "software",
+      label: "Software Asset",
+      caption: "Office 365, Adobe Suite, VPN software",
+    },
+    {
+      value: "network",
+      label: "Network Equipment",
+      caption: "Router, switch, etc.",
+    },
+  ];
+
+  const findOptionByValue = (
+    options: Record<string, string>[],
+    value: string
+  ) => options.find((option) => option.value === value);
 
   return (
     <div className="flex flex-col gap-3">
@@ -36,20 +59,35 @@ const RequestAssetForm = () => {
         control={requestAssetForm.control}
         name="assetType"
         render={({ field }) => (
-          <FormItem>
+          <FormItem className="sm:w-1/2 pr-2">
             <CustomFormLabel required>Asset Type</CustomFormLabel>
             <FormControl>
               <Select value={field.value} onValueChange={field.onChange}>
-                <SelectTrigger className="w-[180px]">
-                  <SelectValue placeholder="Select Asset Type" />
+                <SelectTrigger className="w-full">
+                  <SelectValue
+                    placeholder={
+                      <span className="text-muted-foreground">
+                        Select asset type
+                      </span>
+                    }
+                  >
+                    {field.value
+                      ? findOptionByValue(assetTypes, field.value)?.label
+                      : "Select a category"}
+                  </SelectValue>
                 </SelectTrigger>
                 <SelectContent>
                   <SelectGroup>
-                    <SelectItem value="Hardware Issue">Hardware</SelectItem>
-                    <SelectItem value="Software Issue">Software</SelectItem>
-                    <SelectItem value="Network Issue">
-                      Network Equipment
-                    </SelectItem>
+                    {assetTypes.map((assetType) => (
+                      <SelectItem key={assetType.value} value={assetType.value}>
+                        <div className="flex flex-col">
+                          <span>{assetType.label}</span>
+                          <span className="text-sm text-gray-500">
+                            {assetType.caption}
+                          </span>
+                        </div>
+                      </SelectItem>
+                    ))}
                   </SelectGroup>
                 </SelectContent>
               </Select>
@@ -62,12 +100,12 @@ const RequestAssetForm = () => {
         control={requestAssetForm.control}
         name="assetSpecification"
         render={({ field }) => (
-          <FormItem>
+          <FormItem className="sm:w-1/2 pr-2">
             <CustomFormLabel required>Asset Specs or Model</CustomFormLabel>
             <FormControl>
               <Input
                 {...field}
-                placeholder="Specify the model or type of asset you're requesting (e.g., Dell Latitude 7420, Adobe Creative Cloud)"
+                placeholder="Specify the model or type of asset you're requesting"
               />
             </FormControl>
             <FormMessage />
@@ -82,11 +120,12 @@ const RequestAssetForm = () => {
             <CustomFormLabel required>
               Justification for Request
             </CustomFormLabel>
+            <FormDescription>
+              Provide a justification for why you need this asset (e.g., new
+              project requirement, additional equipment)
+            </FormDescription>
             <FormControl>
-              <Textarea
-                {...field}
-                placeholder="Provide a justification for why you need this asset (e.g., new project requirement, additional equipment)"
-              />
+              <Textarea {...field} placeholder="Enter justification" />
             </FormControl>
             <FormMessage />
           </FormItem>
@@ -96,7 +135,7 @@ const RequestAssetForm = () => {
         control={requestAssetForm.control}
         name="requestDate"
         render={({ field }) => (
-          <FormItem className="w-full sm:w-fit">
+          <FormItem className="w-full">
             <CustomFormLabel>Requested Date for Asset</CustomFormLabel>
             <FormControl>
               <Popover
@@ -107,17 +146,14 @@ const RequestAssetForm = () => {
                   <Button
                     variant={"outline"}
                     className={cn(
-                      "w-full pl-3 text-left font-normal",
+                      "flex w-full sm:w-1/2 pr-2 pl-3 text-left font-normal",
                       !field.value && "text-muted-foreground"
                     )}
                   >
                     {field.value ? (
                       format(field.value, "PPP")
                     ) : (
-                      <span>
-                        Specify a date by which you need the asset, if
-                        applicable.
-                      </span>
+                      <span>Select date</span>
                     )}
                     <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
                   </Button>
