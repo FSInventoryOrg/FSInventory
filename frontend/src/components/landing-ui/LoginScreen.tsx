714f3fa106e43739627182fc3ec00ac1e18dcad5
Yul Sullano <ysullano@fullscale.ph>
2024-11-15 13:49:24 +0800

    Add LoginScreen

---
+++ frontend/src/components/landing-ui/LoginScreen.tsx
@@ -0,0 +1,14 @@
+import LoginForm from "./LoginForm";
+
+export const LoginScreen = () => {
+  return (
+    <div className="flex w-screen h-screen tablet:bg-login-bg tablet:bg-cover">
+      <div
+        className={`flex flex-col justify-center bg-white dark:bg-background-new w-full 
+      tablet:w-[547px] tablet:py-[269px] tablet:px-[72px] mobile-lg:px-[160px] px-6 text-black`}
+      >
+        <LoginForm />
+      </div>
+    </div>
+  );
+};
