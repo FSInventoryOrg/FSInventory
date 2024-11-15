42f91e38b17dfe4e1216d33004c3fedf9cbe4a27
Fritz Gerald Dumdum <fritz.dumdum@redeem.xyz>
2024-11-15 12:40:20 +0800

    Release/staging deployment setup [LINTED]

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import mongoose, { ConnectOptions } from "mongoose";
+import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 
 import userRoutes from "./routes/users";
@@ -19,7 +19,6 @@ import configRoutes from "./system/config";
 import logger from "./utils/logger";
 import swaggerDocs from "./utils/swagger";
 import { startChangeStream } from "./utils/change-stream";
-import path from "path";
 import { auditAssets } from "./utils/common";
 import {
   autoMail,
@@ -39,24 +38,8 @@ const DEFAULT_PORT = 3000;
 const port = Number(process.env.PORT) || DEFAULT_PORT;
 
 const HOST = (process.env.HOST as string) || "localhost";
-const FRONTENDLOC = "../../frontend/dist";
-const NODE_ENV = process.env.NODE_ENV || "development";
 
-const connectOptions = {
-  development: {},
-  staging: {
-    authSource: process.env.MONGODB_AUTH_SOURCE,
-    user: process.env.MONGODB_USERNAME,
-    pass: process.env.MONGODB_PASSWORD,
-    replicaSet: "rs0",
-  },
-  production: {},
-}[NODE_ENV] as ConnectOptions;
-
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string, {
-  dbName: process.env.MONGODB_NAME,
-  ...connectOptions,
-});
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 const db = mongoose.connection;
 
 db.on("connected", () => {
@@ -81,8 +64,6 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, FRONTENDLOC)));
-
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/assets", assetRoutes);
@@ -98,20 +79,9 @@ app.use("/config", configRoutes);
 app.use("/autoMail", autoMailRoutess);
 app.use("/backup", backupRoutes);
 app.use("/version", versionRoutes);
-
-// Catch-all route for unmatched URLs (place it here)
-if (process.env.NODE_ENV !== "development") {
-  app.get("/*", function (req, res) {
-    res.sendFile(
-      path.join(__dirname, `${FRONTENDLOC}/index.html`),
-      function (err) {
-        if (err) {
-          res.status(500).send(err);
-        }
-      }
-    );
-  });
-}
+app.use("/healthcheck", (_, res) =>
+  res.send("Inventory backend is running...")
+);
 
 const isAtlasDB = () => {
   return process.env.MONGODB_CONNECTION_STRING?.startsWith("mongodb+srv://");
