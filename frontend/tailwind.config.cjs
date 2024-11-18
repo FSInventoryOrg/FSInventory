24fd16a4c7cb0b9727a5344b7359dd1d63492943
Yul Sullano <ysullano@fullscale.ph>
2024-11-18 20:41:03 +0800

    Update tailwind config with missing styles

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -114,7 +114,17 @@ module.exports = {
         "gradient-120": "linear-gradient(125.56deg, var(--tw-gradient-stops))",
         "gradient-302": "linear-gradient(302.19deg, var(--tw-gradient-stops))",
         "login-bg":
-          "linear-gradient(164.19deg, rgba(36, 67, 66, 0.4) 11.03%, #244342 88.77%), url('/src/assets/laptop-background.jpg')",
+          "linear-gradient(164.19deg, rgba(36, 67, 66, 0.4) 11.03%, #244342 88.77%), url('./src/assets/laptop-background.jpg')",
+      },
+      screens: {
+        "3xl": "1536px",
+        "2xl": "1400px",
+        tablet: "1024px",
+        "mobile-sm": "320px",
+        "mobile-lg": "640px",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
       },
     },
   },
