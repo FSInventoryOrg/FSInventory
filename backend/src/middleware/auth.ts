d8b9a0b961977ff7294de6e2c9389a5ace1b90e5
Yul Sullano <ysullano@fullscale.ph>
2024-11-19 21:48:40 +0800

    Update auth.ts middleware

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,47 +1,111 @@
+/* eslint-disable @typescript-eslint/no-namespace */
 import { Request, Response, NextFunction } from "express";
-import jwt, { JwtPayload } from 'jsonwebtoken'
 
 export interface UserAuth {
-	userId: string;
-	role: string;
+  userId: number;
+  role: string;
+}
+
+type RocksRoles =
+  | "superadmin"
+  | "admin"
+  | "standarduser"
+  | "client"
+  | "hr"
+  | "floormanager"
+  | "finance"
+  | "sales"
+  | "contentwriter";
+
+export interface RocksUser {
+  id: number;
+  employee_id: number;
+  name: string;
+  email: string;
+  user_name: string;
+  role: RocksRoles;
+  role_is_active: number;
+  can_login: number;
+  is_verified: number;
+  created_at: string;
+  updated_at: string;
+  updated_by: string | null;
+  clients: null;
+  deleted_at: string | null;
+  roles: {
+    data: [
+      {
+        id: number;
+        is_enabled: number;
+        role_id: number;
+        user_id: number;
+      },
+    ];
+  };
 }
 
 /* Extend Request type and add userId */
 declare global {
-	namespace Express {
-		interface Request {
-			user: UserAuth;
-		}
-	}
+  namespace Express {
+    interface Request {
+      user: UserAuth;
+    }
+  }
 }
 
-const verifyToken = (req: Request, res: Response, next: NextFunction) => {
-	const token = req.cookies["auth_token"];
-	if (!token) {
-		return res.status(401).json({ message: "Unauthorized access" })
-	}
-	try {
-		const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
-		req.user = {
-			userId: (decoded as JwtPayload).userId,
-			role: (decoded as JwtPayload).role,
-		} as UserAuth;
-		next();
-	} catch (error) {
-		return res.status(401).json({ message: "Unauthorized access" })
-	}
-}
+const { ROCKS_DEV_API_URL, ROCKS_PRODUCTION_API_URL, NODE_ENV } = process.env;
 
-const verifyRole = (requiredRole: string) => (req: Request, res: Response, next: NextFunction) => {
-	if (!req.user) {
-		return res.status(401).json({ message: "Unauthorized access" });
-	}
-	const { role } = req.user;
-	if (role !== requiredRole) {
-		return res.status(403).json({ message: "Forbidden: Insufficient permissions" });
-	}
-	next();
-}
+const API_URL: string = NODE_ENV
+  ? ROCKS_PRODUCTION_API_URL!
+  : ROCKS_DEV_API_URL!;
+
+const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
+  const token = req.cookies["auth_token"];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized access" });
+  }
+  try {
+    const user: RocksUser = await (await fetch(`${API_URL}/users/me`)).json();
+    req.user = {
+      userId: user.id,
+      role: user.role, // TEMPORARY FIX UNTIL ROCKS API RETURNS ROLES
+    } as UserAuth;
+    next();
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({ message: "Unauthorized access" });
+  }
+};
+
+const verifyRole =
+  (requiredRole: string) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    if (!req.user) {
+      return res.status(401).json({ message: "Unauthorized access" });
+    }
+    const { role } = req.user;
+    if (role !== requiredRole) {
+      return res
+        .status(403)
+        .json({ message: "Forbidden: Insufficient permissions" });
+    }
+    next();
+  };
+
+const tokenStatus = async (token: string) => {
+  const status = await fetch(`${API_URL}/auth/check`, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!status.ok) throw Error("Token expired");
+
+  return status;
+};
 
 export default verifyToken;
-export { verifyRole };
+export { verifyRole, tokenStatus };
