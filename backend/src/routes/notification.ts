8d239e288a3c697a930b8da34820c4a70765d5dc
Yul Sullano <ysullano@fullscale.ph>
2024-11-20 10:45:55 +0800

    Update /notifications routes

--- backend/src/routes/notification.ts
+++ backend/src/routes/notification.ts
@@ -1,91 +1,105 @@
-import express, { Request, Response } from 'express';
-import jwt from "jsonwebtoken";
-import verifyToken from '../middleware/auth';
-import Notification, { NotificationType } from '../models/notification.schema';
-import mongoose from 'mongoose';
+import express, { Request, Response } from "express";
+import verifyToken from "../middleware/auth";
+import Notification, { NotificationType } from "../models/notification.schema";
+import { tokenStatus, tokenUser } from "../middleware/auth";
+import mongoose from "mongoose";
 
 const router = express.Router();
 
-router.get('/', verifyToken, async (req: Request, res: Response) => {
-    try {
-        const token = req.cookies.auth_token;
-        const decodedToken: any = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
-        
-        const notifications: any = await Notification.aggregate().match({
-            $expr: {
-                $and: [
-                    { $in: [decodedToken.userId, '$target_users']}
-                ]
-            }
-        }).sort({
-            updated: -1
-        });
+router.get("/", verifyToken, async (req: Request, res: Response) => {
+  try {
+    const { auth_token: token } = req.cookies;
+    const { data: user } = await (await tokenUser(token)).json();
+    await tokenStatus(token);
 
-        let newData = notifications.reduce((accum: NotificationType[], value: NotificationType, index: number) => {
-            let singleData: any = {
-                _id: value['_id'].toString(),
-                message: value['message'],
-                message_html: value['message_html'],
-                openTab: value['openTab'],
-                url: value['url'],
-                date: value['updated'],
-                read: value['seen_users'].includes(decodedToken.userId)
-            };
+    const notifications: any = await Notification.aggregate()
+      .match({
+        $expr: {
+          $and: [{ $in: [user.id, "$target_users"] }],
+        },
+      })
+      .sort({
+        updated: -1,
+      });
 
+    const newData = notifications.reduce(
+      (accum: NotificationType[], value: NotificationType) => {
+        const singleData: any = {
+          _id: value["_id"].toString(),
+          message: value["message"],
+          message_html: value["message_html"],
+          openTab: value["openTab"],
+          url: value["url"],
+          date: value["updated"],
+          read: value["seen_users"].includes(user.id),
+        };
 
-            accum.push(singleData);
-            return accum;
-        }, []);
+        accum.push(singleData);
+        return accum;
+      },
+      []
+    );
 
-        let notifDetails = {
-            unread: newData.filter((f: any)=> !f['read']).length,
-            count: newData.length,
-            data: newData
-        }
+    const notifDetails = {
+      unread: newData.filter((f: any) => !f["read"]).length,
+      count: newData.length,
+      data: newData,
+    };
 
-        return res.status(200).json(notifDetails);
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: "Something went wrong" });
-    }
+    return res.status(200).json(notifDetails);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
 });
 
-router.patch('/', verifyToken, async (req: Request, res: Response) => {
-    try {
-        const token = req.cookies.auth_token;
-        const decodedToken: any = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
-        
-        const requestBody = req.body;
+router.patch("/", verifyToken, async (req: Request, res: Response) => {
+  try {
+    const { auth_token: token } = req.cookies;
+    const { data: user } = await (await tokenUser(token)).json();
+    await tokenStatus(token);
 
-        if (!Array.isArray(requestBody)) return res.status(422).json({ message: 'Payload should be arrays of _ids' });
-        
-        let mongoIDs = requestBody.reduce((accum, value: string, index) => {
-            let valueID = new mongoose.Types.ObjectId(value);
-            accum.push(valueID);
-            return accum;
-        }, []);
+    const requestBody = req.body;
 
-        const notifications: any = await Notification.aggregate().match({
-            $expr: {
-                $and: [
-                    { $in: [decodedToken.userId, '$target_users']},
-                    { $in: ['$_id', mongoIDs]}
-                ]
-            }
-        });
+    if (!Array.isArray(requestBody))
+      return res
+        .status(422)
+        .json({ message: "Payload should be arrays of _ids" });
 
-        notifications.forEach(async(notif: any) => {
-            notif['seen_users'].push(decodedToken.userId);
-            notif['seen_users'] = notif['seen_users'].filter((value: string, index: number, array: any) => { return array.indexOf(value) === index });
+    const mongoIDs = requestBody.reduce((accum, value: string) => {
+      const valueID = new mongoose.Types.ObjectId(value);
+      accum.push(valueID);
+      return accum;
+    }, []);
 
-            await Notification.updateOne({_id: notif['_id']}, { seen_users: notif['seen_users']})
-        })
+    const notifications: any = await Notification.aggregate().match({
+      $expr: {
+        $and: [
+          { $in: [user.id, "$target_users"] },
+          { $in: ["$_id", mongoIDs] },
+        ],
+      },
+    });
 
-        return res.status(200).json({ message: "Successfully patched" });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: "Something went wrong" });
-    }
+    notifications.forEach(async (notif: any) => {
+      notif["seen_users"].push(user.id);
+      notif["seen_users"] = notif["seen_users"].filter(
+        (value: string, index: number, array: any) => {
+          return array.indexOf(value) === index;
+        }
+      );
+
+      await Notification.updateOne(
+        { _id: notif["_id"] },
+        { seen_users: notif["seen_users"] }
+      );
+    });
+
+    return res.status(200).json({ message: "Successfully patched" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
 });
 
 export default router;
