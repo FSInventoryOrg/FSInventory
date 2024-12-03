77fe324df181227e23cea40ededd84298c84a6d9
Yul Sullano <ysullano@fullscale.ph>
2024-12-03 22:25:03 +0800

    Address comments regarding to/from navigation

--- frontend/src/components/inventory-ui/EditOptions.tsx
+++ frontend/src/components/inventory-ui/EditOptions.tsx
@@ -512,7 +512,11 @@ const EditOptions = ({
         <div
           className="cursor-pointer absolute right-3 top-3 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
           onClick={() => {
-            setOpen(false);
+            if (isCreating) setIsCreating(false);
+            else if (isEditing) setIsEditing(false);
+            else setOpen(false);
+            setNewOption({ property: property, value: "" });
+            setPrefixCode("");
           }}
         >
           <XIcon className="h-4 w-4" />
@@ -702,6 +706,7 @@ const EditOptions = ({
               onClick={() => {
                 setIsEditing(!isEditing);
                 setNewOption({ property: property, value: "" });
+                setPrefixCode("");
               }}
             >
               <ChevronLeftIcon />
